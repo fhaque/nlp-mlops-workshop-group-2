30fc2065d0e70f493394bb04c18b50f8bd37841b
Fatin Haque <2547050+fhaque@users.noreply.github.com>
2020-03-29 20:07:02 -0400

    Added full Twitter app

---
+++ server/model_service.py
@@ -0,0 +1,11 @@
+import mlflow.pyfunc
+import pandas as pd
+
+
+class ModelService():
+    def __init__(self):
+        # self.model = mlflow.pyfunc.load_model('./model')
+        pass
+    
+    def predict(self, data):
+        pass
