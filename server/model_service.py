ec164e34239132ec670cbbc3a6eaf66a438f3451
Fatin Haque <2547050+fhaque@users.noreply.github.com>
2020-03-31 03:47:54 -0400

    switched to Jonad's model

--- server/model_service.py
+++ server/model_service.py
@@ -1,7 +1,9 @@
 # import mlflow.pyfunc
 # import pandas as pd
-from model.data_util import config
-from model.eval import summarise_it
+# from model.data_util import config
+# from model.eval import summarise_it
+import json
+import requests
 
 
 class ModelService():
@@ -10,7 +12,15 @@ class ModelService():
         # self.model = load_model()
         pass
     
+    # def predict(self, data):
+    #     article_summary, out_of_vocab =  summarise_it(data)
+    #     # article_summary, out_of_vocab = predict_from_text(data, self.model)
+    #     return article_summary
+
     def predict(self, data):
-        article_summary, out_of_vocab =  summarise_it(data)
-        # article_summary, out_of_vocab = predict_from_text(data, self.model)
-        return article_summary
+        res = requests.post(
+            'http://ec2-18-224-45-242.us-east-2.compute.amazonaws.com:5000/translator//translate',
+            data = json.dumps([{'src': data, 'id': 100}])
+        )
+        print(res)
+        return res.json()[0][0]["tgt"]
